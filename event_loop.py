08a3ae604295b6ea36c551eaac9ff360db1176fe
JB (work) <ew15dro6k216@opayq.net>
2025-06-11 12:44:59 -0400

    Create basic event loop with state-change monitoring.

---
+++ event_loop.py
@@ -0,0 +1,58 @@
+import class_def
+
+
+Car = class_def.Vehicle(class_def.StarterBatt(), class_def.AuxBatt(), class_def.BatteryCharger())
+
+key_acc_state =     Car.is_acc_powered()
+key_on_state =      Car.is_key_on()
+engine_on_state =   Car.is_engine_running()
+sys_enabled_state = Car.is_enable_switch_closed()
+
+while True:
+    # Check for state changes
+    if Car.is_acc_powered() and not key_acc_state:
+        # Key switched from OFF to ACC
+
+        key_acc_state = True
+        continue
+
+    if not Car.is_acc_powered() and key_acc_state:
+        # Key switched from ACC to OFF
+
+        key_acc_state = False
+        continue
+
+    if Car.is_key_on() and not key_on_state:
+        # Key switched from ACC to ON
+
+        key_on_state = True
+        continue
+
+    if not Car.is_key_on() and key_on_state:
+        # Key switched from ON to ACC
+
+        key_on_state = False
+        continue
+
+    if Car.is_engine_running() and not engine_on_state:
+        # Engine started
+
+        engine_on_state = True
+        continue
+
+    if not Car.is_engine_running() and engine_on_state:
+        # Engine stopped
+
+        engine_on_state = False
+        continue
+
+    if Car.is_enable_switch_closed() and not sys_enabled_state:
+        # Enable switch closed (during previous timeout)
+
+        sys_enabled_state = True
+        continue
+
+    if not Car.is_enable_switch_closed() and sys_enabled_state:
+
+        sys_enabled_state = False
+        continue
