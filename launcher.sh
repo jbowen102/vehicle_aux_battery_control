9e03b0e499df8ea0067a55a09e8abe6b4698cb84
Jonathan <ew15dro6k216@opayq.net>
2025-08-23 12:34:41 -0400

    Kill already-running instances if any exist. Create log file from launcher.sh and use chown to allow user-level program run to modify log.

--- launcher.sh
+++ launcher.sh
@@ -1,8 +1,25 @@
 #!/bin/sh
 
-. /home/user11/.virtualenvs/pimoroni/bin/activate
+USERNAME="user11"
+
+# Activate virtual environment
+. /home/${USERNAME}/.virtualenvs/pimoroni/bin/activate
 # https://stackoverflow.com/a/16011496
 
-cd /home/user11/vehicle_aux_battery_control
+# Kill program if already running.
+killall "python event_loop.py" > /dev/null 2>&1
+# kill $(pgrep -f "python event_loop.py")
+# https://stackoverflow.com/a/40652908
+# https://stackoverflow.com/questions/43724467/what-is-the-difference-between-kill-and-kill-9
+# https://stackoverflow.com/a/617184
+
+# Create log file if it doesn't exist already.
+PROGRAM_ROOT=/home/${USERNAME}/vehicle_aux_battery_control
+TODAY="$(date "+%Y%m%d")"
+LOG_PATH="${PROGRAM_ROOT}/logs/${TODAY}.log"
+touch "${LOG_PATH}"
+# Allow program running from user session to edit same file.
+chown "${USERNAME}" "${LOG_PATH}"
+
+cd "${PROGRAM_ROOT}"
 python event_loop.py
-cd -
