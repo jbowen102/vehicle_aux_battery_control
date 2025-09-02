jbowen102/vehicle_aux_battery_control

commit 7e9159f94c51ff1d8f606828f565d5fb31a7a655

Create separate kill_event_loop.sh script and call from launcher.sh

launcher.sh
@@ -1,19 +1,17 @@
 #!/bin/sh
 
-USERNAME="user11"
+PROGRAM_ROOT=/home/${USERNAME}/vehicle_aux_battery_control
 
+USERNAME=$(cat ${PROGRAM_ROOT}/user)
 # Activate virtual environment
 . /home/${USERNAME}/.virtualenvs/pimoroni/bin/activate
 # https://stackoverflow.com/a/16011496
 
+
 # Kill program if already running.
-sudo kill $(pgrep -f "python event_loop.py") > /dev/null 2>&1
-# https://stackoverflow.com/a/40652908
-# https://stackoverflow.com/questions/43724467/what-is-the-difference-between-kill-and-kill-9
-# https://stackoverflow.com/a/617184
+${PROGRAM_ROOT}/kill_event_loop.sh
 
 # Create log file if it doesn't exist already.
-PROGRAM_ROOT=/home/${USERNAME}/vehicle_aux_battery_control
 TODAY="$(date "+%Y%m%d")"
 LOG_PATH="${PROGRAM_ROOT}/logs/${TODAY}.log"
 touch "${LOG_PATH}"

.gitignore
@@ -16,7 +16,7 @@ TEST*
 # Project-specific
 logs/
 network_names.py
-
+user
 
 # Ignore any file/dir/symlink starting w/ these names:
 reference*

kill_event_loop.sh
@@ -1,0 +1,6 @@
+#!/bin/sh
+
+sudo kill $(pgrep -f "python event_loop.py") > /dev/null 2>&1
+# https://stackoverflow.com/a/40652908
+# https://stackoverflow.com/questions/43724467/what-is-the-difference-between-kill-and-kill-9
+# https://stackoverflow.com/a/617184

commit 331256a6ba9c344a37545131558f590d2fa00572

Replace key-on discrete input with new engine-on signal from ECU. Update control logic to match. Fix bug in launcher.sh introduced by commit 7e9159f.

class_def.py
@@ -15,7 +15,7 @@ SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
 LOG_DIR = os.path.join(SCRIPT_DIR, "logs")
 
 
-ALTERNATOR_OUTPUT_V_MIN = 12.7
+ALTERNATOR_OUTPUT_V_MIN = 12.5
 MAIN_V_MAX = 14.5
 MAIN_V_CHARGED = 12.6
 MAIN_V_MIN = 11.5
@@ -30,8 +30,8 @@ CHARGER_INPUT_SHUNT_LOW_PIN = 0   # labeled 1 on board
 CHARGER_INPUT_SHUNT_HIGH_PIN = 1  # labeled 2 on board
 CHARGER_OUTPUT_PIN = 2            # labeled 3 on board
 
-KEY_ACC_INPUT_PIN = 0             # labeled 1 on board
-KEY_ON_INPUT_PIN = 1              # labeled 2 on board
+ENGINE_ON_INPUT_PIN = 0           # labeled 1 on board
+KEY_ACC_INPUT_PIN = 1             # labeled 2 on board
 ENABLE_SWITCH_DETECT_PIN = 2      # labeled 3 on board
 
 CHARGER_ENABLE_RELAY = 0          # labeled 1 on board
@@ -358,7 +358,7 @@ class Vehicle(object):
         self.BattCharger = BatteryCharger(self.Output)
 
         self.key_acc_detect_pin = KEY_ACC_INPUT_PIN
-        self.key_on_detect_pin = KEY_ON_INPUT_PIN
+        self.engine_on_detect_pin = ENGINE_ON_INPUT_PIN
 
         self.enable_sw_detect_pin = ENABLE_SWITCH_DETECT_PIN
         self.keepalive_relay_num = KEEPALIVE_RELAY
@@ -372,14 +372,11 @@ class Vehicle(object):
         # Returns True when key in either ACC or ON position
         return Controller().is_input_high(self.key_acc_detect_pin)
 
-    def is_key_on(self):
-        return Controller().is_input_high(self.key_on_detect_pin)
-
     def is_key_off(self):
         return not self.is_acc_powered()
 
     def is_engine_running(self):
-        return (self.is_key_on() and (self.get_main_voltage_raw(log=False) >= ALTERNATOR_OUTPUT_V_MIN))
+        return Controller().is_input_high(self.engine_on_detect_pin)
 
     def is_enable_switch_closed(self, log=False):
         # Either ACC power present or keepalive relay should always be powering switch.
@@ -591,11 +588,11 @@ class Vehicle(object):
 
     def charge_aux_batt(self, log=False):
         if not self.is_starter_batt_charged():
-            # Only want to charge with engine running. Sometimes alternator output
-            # varies to where this was incorrectly inferring engine off momentarily
-            # and shutting program down. Switched to just making sure battery charged.
+            # Only want to charge with engine running. Sometimes engine stops after
+            # event loop already called this method (when engine was running), so
+            # instead just make sure FLA batt sufficient.
             main_voltage = self.get_main_voltage(log=True)
-            output_str = "Called Vehicle.charge_aux_batt(), but starter battery insufficiently charged (%.2fV)." % main_voltage
+            output_str = "Called Vehicle.charge_aux_batt(), but engine not running."
             self.Output.print_err(output_str)
             raise ChargeControlError(output_str)
         if self.is_aux_batt_full(log=False):
@@ -605,7 +602,6 @@ class Vehicle(object):
             self.Output.print_warn(output_str)
 
         self.BattCharger.set_charge_direction_rev()
-
         self.BattCharger.enable_charge()
         # self.roll_indicator_light(Controller().light_green_led)
         Controller().toggle_green_led()
@@ -630,8 +626,9 @@ class Vehicle(object):
             self.Output.print_info("Stopped charging.")
 
     def shut_down_controller(self):
-        self.Output.print_warn("Shutting down controller.")
-        Controller().shut_down(delay_s=3)
+        delay = 5
+        self.Output.print_warn("Shutting down controller in %d seconds." % delay)
+        Controller().shut_down(delay_s=delay)
 
 
 class BatteryCharger(object):

event_loop.py
@@ -13,10 +13,9 @@ def main(Output):
     Timer = TimeKeeper(Output, ntp_wait_time=30)
 
     key_acc_powered   = Car.is_acc_powered()
-    key_on_pos        = Car.is_key_on()
     engine_on_state   = Car.is_engine_running()
     sys_enabled_state = Car.is_enable_switch_closed()
-    Output.print_info("Key @ %s." % ("ON" if key_on_pos else ("ACC" if key_acc_powered else "OFF")))
+    Output.print_info("Key %s." % ("@ ACC" if key_acc_powered else "OFF"))
     Output.print_info("Engine %s." % ("ON" if engine_on_state else "OFF"))
     Output.print_info("System %s." % ("enabled" if sys_enabled_state else "disabled"))
 
@@ -68,26 +67,6 @@ def main(Output):
             Timer.start_charge_delay_timer("key ACC -> OFF")
             continue
 
-        elif Car.is_key_on() and not key_on_pos:
-            Output.print_info("Key switched from ACC to ON.")
-            key_on_pos = True
-            Car.stop_charging()
-            Timer.start_charge_delay_timer("key ACC -> ON")
-            continue
-
-        elif not Car.is_key_on() and key_on_pos:
-            Output.print_info("Key switched from ON to ACC.")
-            key_on_pos = False
-            if engine_on_state:
-                Output.print_info("Engine stopped (main voltage raw: %.2f)" % Car.get_main_voltage_raw())
-                engine_on_state = False
-                timer_str = "engine stopped, key ON -> ACC"
-            else:
-                timer_str = "key ON -> ACC"
-            Car.stop_charging()
-            Timer.start_charge_delay_timer(timer_str)
-            continue
-
         elif not Car.is_engine_running() and engine_on_state:
             # Could happen independent of key -> ACC if engine stalls.
             Output.print_info("Engine stopped (main voltage raw: %.2f)" % Car.get_main_voltage_raw())
@@ -116,7 +95,7 @@ def main(Output):
             if engine_on_state:
                 # Key ON, engine running.
                 if first_time_ind:
-                    Output.print_info("State: Key ON, engine running.")
+                    Output.print_info("State: Key ON; engine running.")
                 if Car.is_aux_batt_full(log=first_time_ind):
                     Timer.start_charge_delay_timer("aux battery full already")
                 else:
@@ -125,7 +104,7 @@ def main(Output):
             elif key_acc_powered:
                 # Key in ACC or ON but engine off.
                 if first_time_ind:
-                    Output.print_info("State: Key %s." % ("ON, engine off" if Car.is_key_on() else "in ACC (engine off)"))
+                    Output.print_info("State: Key in ACC or ON; engine off.")
                 if Car.is_aux_batt_sufficient(log=first_time_ind):
                     Car.charge_starter_batt(log=first_time_ind)
                 else:
@@ -141,7 +120,7 @@ def main(Output):
                     Output.print_info("State: Key OFF.")
 
                 # if not Car.is_aux_batt_sufficient(log=first_time_ind):
-                temp_threshold = 13
+                temp_threshold = 13 # temp measure until long-term key-off charge logic implemented.
                 if not Car.is_aux_batt_sufficient(threshold_override=temp_threshold, log=False):
                     # If Li batt V low, power down RPi.
                     Car.is_aux_batt_sufficient(threshold_override=temp_threshold, log=True) # Call again just for logging
@@ -167,11 +146,15 @@ if __name__ == "__main__":
     except TimeoutError:
         # Thrown by automationhat - "Timed out waiting for conversion."
         # Not sure what's causing it yet. Doesn't usually persist across reboot though.
-        Controller().reboot(delay_s=20)
+        delay = 20
+        self.Output.print_warn("Rebooting controller in %d seconds (TimeoutError caught)." % delay)
+        Controller().reboot(delay_s=delay)
     except OSError:
         # Thrown by automationhat - "Device or resource busy"
         # Not sure what's causing it yet. Doesn't usually persist across reboot though.
-        Controller().reboot(delay_s=20)
+        delay = 20
+        self.Output.print_warn("Rebooting controller in %d seconds (OSError caught)." % delay)
+        Controller().reboot(delay_s=delay)
     except KeyboardInterrupt:
         Output.print_shutdown("Keyboard interrupt.")
     except Exception:

launcher.sh
@@ -1,13 +1,11 @@
 #!/bin/sh
 
-PROGRAM_ROOT=/home/${USERNAME}/vehicle_aux_battery_control
-
-USERNAME=$(cat ${PROGRAM_ROOT}/user)
+USERNAME="user11"
 # Activate virtual environment
 . /home/${USERNAME}/.virtualenvs/pimoroni/bin/activate
 # https://stackoverflow.com/a/16011496
 
-
+PROGRAM_ROOT=/home/${USERNAME}/vehicle_aux_battery_control
 # Kill program if already running.
 ${PROGRAM_ROOT}/kill_event_loop.sh
 