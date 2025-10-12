jbowen102/vehicle_aux_battery_control

commit 0a9525e17adae00113765e40e168150eaa5e402e

Fix bug where TimeKeeper.get_time_now() always returned RTC time, causing RTC to never get updated. Add logging when no update needed.

class_def.py
@@ -210,6 +210,11 @@ class TimeKeeper(object):
                 if log:
                     self.Output.print_debug("Updated RTC time (%s -> %s) from NTP-syncd sys time."
                                             % (prev_time, new_time))
+            elif log:
+                self.Output.print_debug("No RTC update needed:")
+                self.Output.print_debug("\tRTC time: %s" % self.get_time_now(string_format=DATETIME_FORMAT, source="rtc"))
+                self.Output.print_debug("\tSys time: %s" % self.get_time_now(string_format=DATETIME_FORMAT, source="sys"))
+
         elif log:
             self.Output.print_debug("Not updating RTC time since sys time not syncd with NTP.")
         # Does not set self.rtc_time_valid to True. Will be False if check_rtc() failed.
@@ -220,7 +225,7 @@ class TimeKeeper(object):
         If source is set to "rtc" or "sys", will use that data source
         regardless of status.
         """
-        if self.rtc_time_valid or source == "rtc":
+        if self.rtc_time_valid and source == "rtc":
             datetime_now = dt.datetime.fromtimestamp(time.mktime(self.rtc.datetime))
         else:
             datetime_now = dt.datetime.now()

commit c7a9f2a9302d35451a9d4e517dfd096d2e5164ee

Fix bug introduced in 0a9525e1 where logs times were defaulting to sys time instead of RTC.

class_def.py
@@ -234,7 +234,7 @@ class TimeKeeper(object):
         If source is set to "rtc" or "sys", will use that data source
         regardless of status.
         """
-        if self.rtc_time_valid and source == "rtc":
+        if self.rtc_time_valid or source == "rtc":
             datetime_now = dt.datetime.fromtimestamp(time.mktime(self.rtc.datetime))
         else:
             datetime_now = dt.datetime.now()
@@ -880,6 +880,7 @@ class Vehicle(object):
         self.Output.print_temp("\tShunt low-side voltage:  %.2f"
                                % Controller().read_voltage(CHARGER_INPUT_SHUNT_LOW_PIN))
         self.Output.print_network_status()
+        self.Output.print_rtc_and_sys_time("Time compare (periodic)")
 
 
 class BatteryCharger(object):