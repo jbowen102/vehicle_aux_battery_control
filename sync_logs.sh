fa8ce061d36c8b53e1a16a86f3f9068ad0124b70
Jonathan <ew15dro6k216@opayq.net>
2025-10-17 15:28:06 -0400

    Add SQL db to backup set in sync_logs.sh.

--- sync_logs.sh
+++ sync_logs.sh
@@ -4,18 +4,32 @@ RPI_PROGRAM_ROOT="/home/${RPI_USER}/vehicle_aux_battery_control"
 
 # If running on RPi, push log BU to USB
 KILLSWITCH_DEV="USB-01"
-DEST_PATH="/media/${RPI_USER}/${KILLSWITCH_DEV}/logs_BU"
+DEST_PATH_LOGS="/media/${RPI_USER}/${KILLSWITCH_DEV}/logs_BU"
+DEST_PATH_DATALOGGING="/media/${RPI_USER}/${KILLSWITCH_DEV}"
 if [ -d "${RPI_PROGRAM_ROOT}" ]; then
-    sleep 15 # need delay for drive to mount.
-    rsync -azi ${RPI_PROGRAM_ROOT}/logs/ ${DEST_PATH}/
+    sleep 15 # need delay after system startup for drive to mount.
+    rsync -azi \
+          ${RPI_PROGRAM_ROOT}/logs/ \
+          ${DEST_PATH_LOGS}/
+
+    rsync -azi --progress \
+          ${RPI_PROGRAM_ROOT}/system_data_log.db \
+          ${DEST_PATH_DATALOGGING}/
 fi
 
 # If running on laptop, pull log BU from RPi
 LAPTOP_USER="user474"
 REMOTE_HOSTNAME="rpi-04"
-DEST_PATH=/home/${LAPTOP_USER}/Storage_Root/Tech/Projects/vehicle_aux_battery_control/logs_BU
+DEST_PATH_LOGS=/home/${LAPTOP_USER}/Storage_Root/Tech/Projects/vehicle_aux_battery_control/logs_BU
+DEST_PATH_DATALOGGING=/home/${LAPTOP_USER}/Storage_Root/Tech/Projects/vehicle_aux_battery_control/datalogging_BU
 if [ -d "${DEST_PATH}" ]; then
-    rsync -azi -e "ssh -i /home/${LAPTOP_USER}/.ssh/id_ed25519" \
-    ${RPI_USER}@${REMOTE_HOSTNAME}:${RPI_PROGRAM_ROOT}/logs/ \
-    ${DEST_PATH}
+    rsync -azivh \
+          -e "ssh -i /home/${LAPTOP_USER}/.ssh/id_ed25519" \
+          ${RPI_USER}@${REMOTE_HOSTNAME}:${RPI_PROGRAM_ROOT}/logs/ \
+          ${DEST_PATH_LOGS}
+
+    rsync -azivh --progress \
+          -e "ssh -i /home/${LAPTOP_USER}/.ssh/id_ed25519" \
+          ${RPI_USER}@${REMOTE_HOSTNAME}:${RPI_PROGRAM_ROOT}/system_data_log.db \
+          ${DEST_PATH_DATALOGGING}/
 fi
