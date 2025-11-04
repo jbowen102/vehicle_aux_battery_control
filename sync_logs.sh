584df1252d5887886f4fc828e8ebeae2fc78d6f2
Jonathan <ew15dro6k216@opayq.net>
2025-11-04 12:31:11 -0500

    Add partials dir for log and datalog remote-backup rsync jobs.

--- sync_logs.sh
+++ sync_logs.sh
@@ -20,15 +20,19 @@ fi
 # If running on laptop, pull log BU from RPi
 LAPTOP_USER="user474"
 REMOTE_HOSTNAME="rpi-04"
-DEST_PATH_LOGS=/home/${LAPTOP_USER}/Storage_Root/Tech/Projects/vehicle_aux_battery_control/logs_BU
-DEST_PATH_DATALOGGING=/home/${LAPTOP_USER}/Storage_Root/Tech/Projects/vehicle_aux_battery_control/datalogging_BU
+DEST_PATH_BASE=/home/${LAPTOP_USER}/Storage_Root/Tech/Projects/vehicle_aux_battery_control
+DEST_PATH_LOGS=${DEST_PATH_BASE}/logs_BU
+DEST_PATH_DATALOGGING=${DEST_PATH_BASE}/datalogging_BU
 if [ -d "/home/${LAPTOP_USER}" ]; then
     rsync -azivh \
+          --partial-dir="${DEST_PATH_BASE}/rsync_partials_buffer" \
           -e "ssh -i /home/${LAPTOP_USER}/.ssh/id_ed25519" \
           ${RPI_USER}@${REMOTE_HOSTNAME}:${RPI_PROGRAM_ROOT}/logs/ \
           ${DEST_PATH_LOGS}
 
-    rsync -azivh --progress \
+    rsync -azivh \
+          --progress \
+          --partial-dir="${DEST_PATH_BASE}/rsync_partials_buffer" \
           -e "ssh -i /home/${LAPTOP_USER}/.ssh/id_ed25519" \
           ${RPI_USER}@${REMOTE_HOSTNAME}:${RPI_PROGRAM_ROOT}/system_data_log.db \
           ${DEST_PATH_DATALOGGING}/
